aba0c96a69cf17c83bc18c6d052760cce26a5776
Me <gekoncze@seznam.cz>
2020-11-14 17:00:02 +0100

    added cache annotation

---
+++ src/cz/mg/annotations/storage/Cache.java
@@ -0,0 +1,19 @@
+package cz.mg.annotations.storage;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+// Applicable only to entity fields.
+// Same as Part.
+// Indicates that the field holds data required only for optimization.
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+    ElementType.FIELD,
+    ElementType.TYPE_USE,
+    ElementType.PARAMETER
+})
+public @interface Cache {
+}
