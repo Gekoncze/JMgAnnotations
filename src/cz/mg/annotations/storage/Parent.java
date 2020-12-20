50aebc0b5358e7c48d0d05609d4b39b596a6d52a
Me <gekoncze@seznam.cz>
2020-12-20 21:39:41 +0100

    added parent annotation

---
+++ src/cz/mg/annotations/storage/Parent.java
@@ -0,0 +1,20 @@
+package cz.mg.annotations.storage;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+// Applicable only to entity fields.
+// Annotated field just holds reference to its parent object (no ownership).
+@Retention(RetentionPolicy.RUNTIME)
+@Target({
+    ElementType.FIELD,
+    ElementType.LOCAL_VARIABLE,
+    ElementType.METHOD,
+    ElementType.PARAMETER,
+    ElementType.TYPE_USE
+})
+public @interface Parent {
+}
