7e3448103a1b159d4605fe3ba3d1335af8d62153
mg <gekoncze@seznam.cz>
2022-02-16 16:38:56 +0100

    Added Collection and Test annotations.

---
+++ src/cz/mg/annotations/classes/Collection.java
@@ -0,0 +1,12 @@
+package cz.mg.annotations.classes;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Collection {
+}
