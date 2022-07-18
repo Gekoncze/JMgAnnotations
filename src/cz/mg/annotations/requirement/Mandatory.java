05fb740e73a7ad37978aed5327789e1571d6822b
Mg <gekoncze@seznam.cz>
2022-07-18 19:25:53 +0200

    Renamed Storage annotation to Group annotation.

--- src/cz/mg/annotations/requirement/Mandatory.java
+++ src/cz/mg/annotations/requirement/Mandatory.java
@@ -6,9 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-// Applicable only to entity fields.
-// Annotated field must not be null.
-// If applied to collection, then it is the same as if CollectionMandatory and ItemsMandatory annotations were applied.
 @Retention(RetentionPolicy.RUNTIME)
 @Target({
     ElementType.FIELD,
--- src/cz/mg/annotations/requirement/Optional.java
+++ src/cz/mg/annotations/requirement/Optional.java
@@ -6,9 +6,6 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-// Applicable only to entity fields.
-// Annotated field can be null.
-// If applied to collection, then it is the same as if CollectionOptional and ItemsOptional annotations were applied.
 @Retention(RetentionPolicy.RUNTIME)
 @Target({
     ElementType.FIELD,
